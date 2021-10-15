f86f00c7a518b904d07a1c89e8ace055986f3785
ItDevMikhail <90688212+ItDevMikhail@users.noreply.github.com>
2021-10-15 14:51:06 +0300

    add training todolist

---
+++ reactNativeExpoNoTS/components/form.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import { StyleSheet, Text, TextInput, Button, View } from "react-native";
+
+export default function Form({ addHandler }) {
+    const [text, setText] = useState('');
+
+    const onChange = (text) => {
+        setText(text);
+    }
+
+    return (
+        <View>
+            <TextInput style={styles.input} onChangeText={onChange} placeholder='Впишите задачу' />
+            <Button title='Добавит задачу' color='green' onPress={() => { addHandler(text) }} />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    input: {
+        borderBottomWidth: 1,
+        borderColor: '#000',
+        padding: 10,
+        marginVertical: 30,
+        marginHorizontal: '20%',
+        width: '60%'
+    }
+});
